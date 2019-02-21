8baca7bfcec6d2af284ff909e69a3b94725b5383
andrewmherren <andrew.herren@gmail.com>
2019-02-21 09:18:03 -0800

    incorperate store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles/indexStyles.styl'
 export modalActions from './actions/modalActions'
 export modalReducer from './reducers/modalReducer'
 export ModalFramework from './components/ModalFramework.jsx'
+export modalStore from './utility/modalStore'
 
-//# sourceMappingURL=../dist/modal-framework.js.map
+//# sourceMappingURL=/dist/modal-framework.js.map
---
+++ src/utility/modalStore.js
@@ -0,0 +1,3 @@
+// kind of wasteful here but this is in fact the store and this is how we do
+// other packages so something to be said for consistency
+export default []
